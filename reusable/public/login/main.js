d77f935d5617e1495d1e744a2e0a2ccc64488e6a
chinmoy33 <149528949+chinmoy33@users.noreply.github.com>
2025-03-03 22:38:49 +0530

    Delete reusable/public directory

--- reusable/public/login/main.js
+++
@@ -1,8 +0,0 @@
-const result=document.querySelector(".result");
-
-async function displayresult(){
-    const {data}=await axios.post("/api/v1/project/login");
-    result.innerHTML=`<p>${data.msg}</p>`;
-}
-
-displayresult();
